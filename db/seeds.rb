1c2f27efcbd164b54b663a8a6030c7f824018a7c
hortencia718 <hortenciaecisneros@gmail.com>
2020-08-20 18:37:15 -0400

    upseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,27 @@
 User.destroy_all
 Joke.destroy_all
 
-user1 = User.create(username: "mazzy")
-user2 = User.create(username: "fuzzy")
+user1 = User.create(username: "bizzy")
+user2 = User.create(username: "fizzy")
 user3 = User.create(username: "gizzy")
-user4 = User.create(username: "wizzy")
+user4 = User.create(username: "vizzy")
 
 # here we are create a new user and save it to variable 
 # (user1)
 
-joke1 = Joke.create(content: " Why did the clock get kicked out of the library?")
-# b/c it tocked to much
-joke2 = Joke.create(content: " what button is impossible to unbutton?")
-    # the belly button
+joke1 = Joke.create(content: " Why did the clock get kicked out of the library? because it tocked to much")
+
+joke2 = Joke.create(content: " what button is impossible to unbutton? the belly button")
+
 joke3 = Joke.create(content: " how do you get a squirrel to like you? act like a nut ")
-# act like a nut 
-joke4 = Joke.create(content: "what did the traffic light say to the car")
+
+joke4 = Joke.create(content: "what did the traffic light say to the car dont look i'm about to change")
 # "dont look! 'i'm about to change"
 # create a new "joke " and save its content ""
-user_joke= UserJoke.create(user_id: user3.id, joke_id: joke2.id)
+user_joke= UserJoke.create(user_id: user4.id, joke_id: joke4.id)
 user_joke= UserJoke.create(user_id: user2.id, joke_id: joke2.id)
 user_joke= UserJoke.create(user_id: user3.id, joke_id: joke3.id)
+user_joke= UserJoke.create(user_id: user1.id, joke_id: joke1.id)
 
 
 
